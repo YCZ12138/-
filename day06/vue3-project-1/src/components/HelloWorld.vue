c7707ddc3cd78fce8279d87f71f3ce88ae90b3de
任殿臣 <chen_2019@qq.com>
2022-07-01 09:08:55 +0800

    提交day06的代码和笔记

--- day06/vue3-project-1/src/components/HelloWorld.vue
+++ day06/vue3-project-1/src/components/HelloWorld.vue
@@ -1,48 +1,56 @@
+<!--
+ * @Description: 
+ * @Author: rendc
+ * @Date: 2022-06-30 09:27:46
+ * @LastEditors: rendc
+ * @LastEditTime: 2022-06-30 11:49:41
+-->
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <VanButton type="primary">主要按钮</VanButton>
+    <van-cell-group inset>
+      <van-cell
+        title="单元格"
+        value="内容"
+      />
+      <van-cell
+        title="单元格"
+        value="内容"
+        label="描述信息"
+      />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import { defineComponent } from "@vue/runtime-core";
+import { Button, Cell, CellGroup } from "vant";
+
+// export default {
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String
+//   }
+// }
+
+export default defineComponent({
+  components: {
+    VanButton: Button,
+    VanCell: Cell,
+    VanCellGroup: CellGroup,
+  },
+  setup() {
+    return {};
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+.hello {
+  background-color: rgb(247, 248, 250);
+}
 h3 {
   margin: 40px 0 0;
 }
